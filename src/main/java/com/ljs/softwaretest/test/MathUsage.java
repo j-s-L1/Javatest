5ffe1cdc2b109410f90b5af50a3b9a5247065a9a
Ljs <Ljs@gg.com>
2023-05-31 20:45:12 +0800

    first commit

---
+++ src/main/java/com/ljs/softwaretest/test/MathUsage.java
@@ -0,0 +1,9 @@
+package com.ljs.softwaretest.test;
+
+/**
+ * @author : luo
+ * @date : 2023/5/31 - 20:36
+ * @desc :
+ */
+public class MathUsage {
+}
---
+++ src/test/java/com/ljs/software/test/MathUsageTest.java
@@ -0,0 +1,9 @@
+package com.ljs.software.test;
+
+/**
+ * @author : luo
+ * @date : 2023/5/31 - 20:36
+ * @desc :
+ */
+public class MathUsageTest {
+}
